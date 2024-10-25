685eb68c7056ed77a5da472320e554a0b0b7cb85
Andres <andresruse18@gmail.com>
2024-10-25 16:18:07 +0200

    add : addRecord; getRecord; makeMigrations

--- include/model/model.h
+++ include/model/model.h
@@ -2,38 +2,81 @@
 
 #include <string>
 #include <vector>
+#include <memory>
+#include <sstream>
+#include <unordered_map>
+#include <libpq-fe.h>
 
-#include "model/types.h"
+#include "model/fields.h"
 #include "db/connection.h"
+#include "model/structs.h"
+
 
 namespace illion {
 
     class Model {
         public:
+
+            // Constructor
             Model();
             ~Model();
 
+            // Methods
             std::string getModelName();
             std::string getAppName();
+            std::string getTableName();
 
+            bool fieldExistsInDB(const std::string& fieldName);
+
+            void save();
+            void makeMigrations();
             void setAppName(std::string name);
             void setModelName(std::string name);
+            void addRecord(const std::unordered_map<std::string, std::string>& record);
 
-            std::string getTableName();
-            void addField(std::vector<illion::Field>);
-            void save();
+            template<typename T>
+            std::unordered_map<std::string, std::string> getRecord(const std::string& fieldName, T match) {
+
+                std::ostringstream sql;
+                sql << "SELECT * FROM " << getTableName() << " WHERE " << fieldName << " = ";
+
+                if constexpr (std::is_same<T, std::string>::value) {
+                    sql << "'" << match << "'";
+                } else if constexpr (std::is_same<T, bool>::value) {
+                    sql << (match ? "true" : "false");
+                } else {
+                    sql << match;
+                }
+
+                sql << ";";
+
+                return executeGetQuery(sql.str());
+            }
+            // void updateField(const std::string& fieldName, const std::string& value);
 
         private:
-            PGconn* getConnection();
 
-            void createTable();
-
+            // Attributes
+            bool isTableCreated;
+            PGconn* conn;
             std::string appName;
             std::string modelName;
-            std::vector<illion::Field> fields;
-            std::vector<illion::Field> fieldsToAdd;
 
-            bool isTableCreated;
+            // Methods
+            PGconn* getConnection();
+
+            void setConnection();
+            void clearConnection();
+
+            std::unordered_map<std::string, std::string> executeGetQuery(const std::string& sql);
+            ExecuteResult executeSQL(std::string sql, std::string errorMessage, bool throwError = true, bool clearRes = true);
+
+        protected:
+            // Attributes
+            std::vector<illion::Field> fields;
+
+            void addColumns();
+            void createTable();
     };
 
 }
--- src/model/model.cpp
+++ src/model/model.cpp
@@ -3,14 +3,20 @@
 #include <stdexcept>
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 #include "model/model.h"
-#include "model/types.h"
+#include "model/fields.h"
+#include "model/structs.h"
 
 namespace illion {
 
-    Model::Model() : isTableCreated{}, fieldsToAdd{} {};
-    Model::~Model() {};
+    Model::Model() : isTableCreated(false), conn(nullptr) {
+        setConnection();
+    }
+    Model::~Model() {
+        clearConnection();
+    }
 
     std::string Model::getTableName() {
         return appName + "__" + modelName;
@@ -32,6 +38,14 @@ namespace illion {
         modelName = name;
     }
 
+    void Model::setConnection() {
+        conn = getConnection();
+    }
+
+    void Model::clearConnection() {
+        conn = nullptr;
+    }
+
     PGconn* Model::getConnection() {
         PGconn* conn = Connection::getInstance().getConnection();
         if (conn == nullptr) throw std::runtime_error("No valid database connection");
@@ -39,41 +53,108 @@ namespace illion {
         return conn;
     }
 
-    void Model::addField(std::vector<illion::Field> fields) {
-
-        for (illion::Field f : fields) {
-            fieldsToAdd.push_back(f);
-        }
-
-    }
-
-    void Model::createTable() {
-
-        if (isTableCreated) return;
-
-        PGconn* conn = getConnection();
-        std::string sql = "CREATE TABLE " + getTableName() + " (id SERIAL PRIMARY KEY);";
+    illion::ExecuteResult Model::executeSQL(std::string sql, std::string errorMessage, bool throwError, bool clearRes) {
 
         PGresult* res = PQexec(conn, sql.c_str());
+        bool success = PQresultStatus(res) == PGRES_COMMAND_OK || PQresultStatus(res) == PGRES_TUPLES_OK;
+        if (clearRes) PQclear(res);
 
-        if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-            std::cerr << "Error: " << PQerrorMessage(conn) << "\n";
-            throw std::runtime_error("Table creation failed");
+        if (!success) {
+            if (throwError) {
+                throw std::runtime_error(errorMessage + ": " + PQerrorMessage(conn));
+            } else {
+                std::cerr << "Error while executing SQL: " << PQerrorMessage(conn) << "\n";
+            }
         }
 
-        std::cerr << "Table created successfully \n";
+        return illion::ExecuteResult{success, PQerrorMessage(conn), res};
+    }
 
-        PQclear(res);
-        isTableCreated = true;
+
+
+    std::unordered_map<std::string, std::string> Model::executeGetQuery(const std::string& sql) {
+
+        illion::ExecuteResult result = executeSQL(sql, "Error executing query", true, false);
+
+        int numRows = PQntuples(result.result);
+
+        if (numRows == 0) {
+            PQclear(result.result);
+            throw std::runtime_error("No records found");
+        }
+        if (numRows > 1) {
+            PQclear(result.result);
+            throw std::runtime_error("Multiple records found");
+        }
+
+        // Get the values
+        std::unordered_map<std::string, std::string> record;
+        for (int i = 0; i < PQnfields(result.result); i++) {
+            record[PQfname(result.result, i)] = PQgetvalue(result.result, 0, i);
+        }
+
+        PQclear(result.result);
+        return record;
+    }
+
+    void Model::addRecord(const std::unordered_map<std::string, std::string>& record) {
+
+        std::stringstream sql;
+
+        sql << "INSERT INTO " << getTableName() << " (";
+
+        // Adding the column names
+        for (auto it = fields.begin(); it != fields.end(); ++it) {
+            sql << it->name;
+            if (std::next(it) != fields.end()) sql << ", ";
+        }
+
+        sql << ") VALUES (";
+
+        // Add values passed or default values
+        for (auto it = fields.begin(); it != fields.end(); ++it) {
+            auto valueIt = record.find(it->name);
+            if (valueIt != record.end()) {
+
+                // This mean that we have a value for this field
+                sql << "'" << valueIt->second << "'";
+            } else {
+                // We use the default value
+                sql << it->defaultValue;
+            }
+
+            if (std::next(it) != fields.end()) sql << ", ";
+        }
+
+        sql << ");";
+
+        std::string errorMessage = "Error adding record to " + getTableName();
+        executeSQL(sql.str(), errorMessage);
+
+        std::cout << "Record added successfully \n";
 
     }
 
-    void Model::save() {
-        if (!isTableCreated) createTable();
-
+    bool Model::fieldExistsInDB(const std::string& fieldName) {
         PGconn* conn = getConnection();
 
-        for (const illion::Field& field : fieldsToAdd) {
+        // Query to check if the column exists in the table
+        std::string sql = "SELECT column_name FROM information_schema.columns WHERE table_name = '" + getTableName() + "' AND column_name = '" + fieldName + "';";
+
+        PGresult* res = PQexec(conn, sql.c_str());
+        bool exists = (PQntuples(res) > 0);
+        PQclear(res);
+
+        return exists;
+
+    }
+
+    void Model::addColumns() {
+
+        for (const illion::Field& field : fields) {
+
+            if (fieldExistsInDB(field.name)) continue;
+
             auto it = fieldTypeToString.find(field.type);
 
             if (it == fieldTypeToString.end()) throw std::runtime_error("Unknown field type");
@@ -81,21 +162,35 @@ namespace illion {
             std::string fieldType = it->second;
             std::string sql = "ALTER TABLE " + getTableName() + " ADD COLUMN " + field.name + " " + fieldType;
 
-            PGresult* res = PQexec(conn, sql.c_str());
-
-            if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-                std::cerr << "Error adding field: " << PQerrorMessage(conn) << "\n";
-                PQclear(res);
-                throw std::runtime_error("Failed to add field: " + field.name);
-            }
-
-            PQclear(res);
+            std::string errorMessage = "Error Adding field: " + field.name;
+            executeSQL(sql, errorMessage);
             std::cerr << "Field: '" << field.name << "' added successfully \n";
-        }
 
-        fieldsToAdd.clear();
+        }
     }
 
+    void Model::createTable() {
 
+        if (isTableCreated) return;
 
+        std::string sql = "CREATE TABLE " + getTableName() + " (id SERIAL PRIMARY KEY);";
+
+        executeSQL(sql, "Table creation failed");
+
+        std::cerr << "Table created successfully \n";
+
+        isTableCreated = true;
+
+    }
+
+    void Model::save() {}
+
+    void Model::makeMigrations() {
+        setConnection();
+
+        createTable();
+        addColumns();
+
+        clearConnection();
+    }
 }
