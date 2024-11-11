8b74818d595b047428e2e528cb24f6b16cca3f94
Andres <andresruse18@gmail.com>
2024-11-11 11:28:16 +0100

    create injection protection class

--- include/sql/helper.h
+++ include/sql/helper.h
@@ -1,5 +1,8 @@
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <stdexcept>
 
 namespace SqlHelper {
     template<typename T>
@@ -36,4 +39,147 @@ namespace SqlHelper {
 
         return sql.str();
     }
+
+    class AntiSQLInjection {
+
+        private:
+
+            bool checkSet {};
+            bool checkDrop {};
+            bool checkComma {};
+            bool checkAlter {};
+            bool checkSelect {};
+            bool checkWhiteSpaces {};
+            bool checkSemiColom {};
+
+            bool isValidSQL {};
+            bool isTextValidated {};
+            bool (AntiSQLInjection::*checkMethods[6])();
+
+            std::string textToEvaluate {};
+            std::string upperTextToEvaluate {};
+
+            static AntiSQLInjection* instancePtr;
+
+            // METHODS
+            void setTextToEvaluate(std::string& text) {
+
+                textToEvaluate = text;
+                upperTextToEvaluate = text;
+
+                std::transform(upperTextToEvaluate.begin(), upperTextToEvaluate.end(), upperTextToEvaluate.begin(), [](unsigned char c) {
+                    return std::toupper(c);
+                });
+            }
+
+
+        public:
+            AntiSQLInjection(
+                std::string textToEvaluate,
+                bool checkSet = true,
+                bool checkDrop = true,
+                bool checkComma = true,
+                bool checkAlter = true,
+                bool checkSelect = true,
+                bool checkWhiteSpaces = true,
+                bool checkSemiColom = true,
+                bool throwException = true
+            ) :
+            checkMethods  {
+                &AntiSQLInjection::containsWhiteSpaces,
+                &AntiSQLInjection::containsComma,
+                &AntiSQLInjection::containsDrop,
+                &AntiSQLInjection::containsAlter,
+                &AntiSQLInjection::containsSet,
+                &AntiSQLInjection::containsSelect
+            }
+            {
+                this->checkSet = checkSet;
+                this->checkDrop = checkDrop;
+                this->checkComma = checkComma;
+                this->checkAlter = checkAlter;
+                this->checkSelect = checkSelect;
+                this->checkWhiteSpaces = checkWhiteSpaces;
+                this->checkSemiColom = checkSemiColom;
+
+                setTextToEvaluate(textToEvaluate);
+
+                runChecks();
+                if (!getIsValidSQL()) throw std::runtime_error("SQL INJECTION DETECTED");
+
+            };
+
+        bool getIsValidSQL() {
+            if (!isTextValidated) throw std::runtime_error("AntiSQLInjection::runChecks must be called first");
+            return isValidSQL;
+
+        }
+
+        bool runChecks() {
+
+            if (isTextValidated) return isValidSQL;
+
+            isValidSQL = true;
+            isTextValidated = true;
+
+            for (auto checker : checkMethods) {
+                bool result = (this->*checker)();
+
+                if (result) {
+                    isValidSQL = false;
+                    break;
+                }
+            }
+
+            return isValidSQL;
+
+        }
+
+        bool containsSymbol(std::string symbol) {
+
+            size_t found = upperTextToEvaluate.find(symbol);
+            if (found != std::string::npos) return false;
+
+            return true;
+
+        }
+
+        bool containsSet() {
+            if (!checkSet) return false;
+            return containsSymbol("SET");
+        }
+
+        bool containsDrop() {
+            if (!checkDrop) return false;
+            return containsSymbol("DROP");
+        }
+
+        bool containsComma() {
+            if (!checkComma) return false;
+            return containsSymbol(",");
+        }
+
+        bool containsAlter() {
+            if (!checkAlter) return false;
+            return containsSymbol("ALTER");
+        }
+
+        bool containsSelect() {
+            if (!checkSelect) return false;
+            return containsSymbol("SELECT");
+        }
+
+        bool containsWhiteSpaces() {
+            if (!checkWhiteSpaces) return false;
+            return containsSymbol(" ");
+        }
+
+        bool containsWhiteSemiColom() {
+            if (!checkSemiColom) return false;
+            return containsSymbol(";");
+        }
+
+    };
+
 }
+
